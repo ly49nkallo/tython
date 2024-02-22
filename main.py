049c0a7c47cd087951a132c9666492acea0f6cf5
ly49nkallo <tyabrennan@gmail.com>
2024-02-22 00:46:26 -0800

    r

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 '''Author: Ty Brennan'''
 '''Tython compiler'''
-import regex as re
 import os, sys
 import pathlib
 import functools
@@ -14,6 +13,8 @@ from pprint import pprint
 
 LOGFILE:pathlib.Path = pathlib.Path("./log.log")
 VERSION:str = "0.1.0"
+
+COMMENT_DELIM = '#'
 
 class TOKEN_TYPE(enum.Enum):
         INT32 = enum.auto()
@@ -37,42 +38,68 @@ class Token(object):
 
     def __repr__(self):
         ret = str(self.type)
-        if self.value: ret += str(self.value)
+        if self.value: ret += f":{self.value}"
         return ret
 
 class Lexer(object):
     '''Responsible for taking raw text input and generating a list of tokens.'''
-    def __init__(self, text):
-        self.text = text
-        self.pos = -1
-        self.current_char = None
+
+    @classmethod
+    def __call__(cls, text:str) -> list:
+        lines = text.split('\n')
+        # remove blank lines
+        lines = list(filter(lambda x: x != '', lines))
+        buffers = []
+        for line in lines:
+            buffer = ''
+            i = 0
+            in_str_lit:bool = False
+            while i < len(line):
+                curr_char = line[i]
+                next_char = line[i+1] if i+1<len(line) else None
+                prev_char = line[i-1] if i-1>=0 else None
+                buffer += curr_char
+                if curr_char == COMMENT_DELIM:
+                    pass
+                elif curr_char == ' ':
+                    buffer = ''
+                elif next_char == COMMENT_DELIM:
+                    buffers.append(buffer)
+                    buffer = ''
+                    break
+                elif next_char == ' ' and not in_str_lit:
+                    buffers.append(buffer)
+                    buffer = ''
+                elif next_char == '\"':
+                    buffers.append(buffer)
+                    buffer = ''
+                    in_str_lit = not in_str_lit
+                i += 1
+        # buffers = list(filter(lambda x: x != '' and x != ' ', buffers))
+        pprint(buffers)
         pass
-    
-    def __call__(self):
+
+    @classmethod
+    def tokenize_buffer(cls, buffer:str):
         pass
-    
-    def advance(self):
-        self.pos += 1
-    
-    def digest(self, line):
-        pass
-    
+
 def main() -> None:
     logger:logging.Logger = logging.getLogger()
-    parser = argparse.ArgumentParser(description=f'TYTHON Compiler {VERSION}')
-    parser.add_argument('input_file')
-    parser.add_argument('-o', '--output')
-    parser.add_argument('-c', '--compile', action='store_true')
-    parser.add_argument('-i', '--interpret', action='store_true')
-    parser.add_argument('-v', '--version', action='version', version=f'Tython {VERSION}')
-    args = parser.parse_args() 
-    filepath:pathlib.Path = pathlib.Path(args.input_file)
+    if len(sys.argv) == 1: #bypass 
+        filepath = pathlib.Path("./test_programs/hello.ty")
+    else:
+        parser = argparse.ArgumentParser(description=f'TYTHON Compiler {VERSION}')
+        parser.add_argument('input_file')
+        parser.add_argument('-o', '--output')
+        parser.add_argument('-c', '--compile', action='store_true')
+        parser.add_argument('-i', '--interpret', action='store_true')
+        parser.add_argument('-v', '--version', action='version', version=f'Tython {VERSION}')
+        args = parser.parse_args()
+        filepath:pathlib.Path = pathlib.Path(args.input_file)
+
     with open(filepath, 'r') as f:
         file_contents = f.read()
-    print(file_contents)
 
-    exit(0)
-    
     lexer = Lexer()
     tokens = lexer(file_contents)
 
