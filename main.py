b87387fc563bfbf9249ca40b509e33fc220023f7
Ty Brennan <65131672+ly49nkallo@users.noreply.github.com>
2024-02-21 16:50:19 -0800

    more

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import typing
 from pprint import pprint
 
 LOGFILE:pathlib.Path = pathlib.Path("./log.log")
+VERSION:str = "0.1.0"
 
 class TOKEN_TYPE(enum.Enum):
         INT32 = enum.auto()
@@ -28,6 +29,7 @@ class TOKEN_TYPE(enum.Enum):
         R_PAREN = enum.auto()
 
 class Token(object):
+    '''A lexical unit of code correspinding to a certain, specific function.'''
     def __init__(self, type:TOKEN_TYPE, value):
         assert isinstance(type, TOKEN_TYPE)
         self.type:TOKEN_TYPE = type
@@ -39,25 +41,40 @@ class Token(object):
         return ret
 
 class Lexer(object):
+    '''Responsible for taking raw text input and generating a list of tokens.'''
     def __init__(self, text):
         self.text = text
         self.pos = -1
         self.current_char = None
         pass
-
+    
+    def __call__(self):
+        pass
+    
     def advance(self):
         self.pos += 1
-     
+    
+    def digest(self, line):
+        pass
+    
 def main() -> None:
     logger:logging.Logger = logging.getLogger()
-    parser = argparse.ArgumentParser(description='TYTHON Compiler')
+    parser = argparse.ArgumentParser(description=f'TYTHON Compiler {VERSION}')
     parser.add_argument('input_file')
+    parser.add_argument('-o', '--output')
+    parser.add_argument('-c', '--compile', action='store_true')
+    parser.add_argument('-i', '--interpret', action='store_true')
+    parser.add_argument('-v', '--version', action='version', version=f'Tython {VERSION}')
     args = parser.parse_args() 
     filepath:pathlib.Path = pathlib.Path(args.input_file)
     with open(filepath, 'r') as f:
-        #use 'os.linesep' if it doesn't work but python doesn't care for some reason
-        file_contents = f.read().split('\n') 
-    pprint(file_contents)
+        file_contents = f.read()
+    print(file_contents)
+
+    exit(0)
+    
+    lexer = Lexer()
+    tokens = lexer(file_contents)
 
     pass
 
