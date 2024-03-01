97eebbc66fadda15174087968265831837d832b6
Ty Brennan <65131672+ly49nkallo@users.noreply.github.com>
2024-03-01 00:15:34 -0800

    parser

--- main.py
+++ main.py
@@ -47,6 +47,7 @@ class TOKEN_TYPE(enum.Enum):
         EQUAL_TO = '=='
         # PROGRAM CONTROL
         PROGRAM = 'PROGRAM'
+        VERSION = 'VERSION'
         IMPLICIT = 'IMPLICIT'
         CALL = 'CALL'
         # VARIABLE LITERALS
@@ -83,10 +84,10 @@ class TOKEN_TYPE(enum.Enum):
 
 REQUIRES_VALUE:set = {
         TOKEN_TYPE.CHAR_LIT,
-        TOKEN_TYPE.STR_LIT, 
-        TOKEN_TYPE.INT_LIT, 
-        TOKEN_TYPE.FLOAT_LIT, 
-        TOKEN_TYPE.HEX_LIT, 
+        TOKEN_TYPE.STR_LIT,
+        TOKEN_TYPE.INT_LIT,
+        TOKEN_TYPE.FLOAT_LIT,
+        TOKEN_TYPE.HEX_LIT,
         TOKEN_TYPE.BIN_LIT,
         TOKEN_TYPE.VAR,
         TOKEN_TYPE.ARRAY_VAR,
@@ -95,14 +96,16 @@ REQUIRES_VALUE:set = {
 
 class Token(object):
     '''A lexical unit of code correspinding to a certain, specific function.'''
-    def __init__(self, type:TOKEN_TYPE, value=None):
+    def __init__(self, type:TOKEN_TYPE, line_number:int, value=None):
         assert isinstance(type, TOKEN_TYPE)
         self.type:TOKEN_TYPE = type
         self.value = value
+        self.line_number:int = line_number
 
     def __repr__(self) -> str:
         ret = str(self.type)
         if self.value: ret += f":{self.value}"
+        if DEBUG: ret += f":line {self.line_number}"
         return ret
 
 class Node(object):
@@ -114,71 +117,77 @@ class Node(object):
     def is_leaf(self) -> bool:
         return (len(self.children) == 0)
 
+    def append(self, other:'Node'):
+        assert isinstance(other, Node)
+        self.children.append(other)
+
     def __repr__(self):
-        if name: ret = name
+        if self.name is not None: ret = self.name
         else: ret = repr(self.token) + '\n'
         for c in self.children:
-            msg += '    ' + repr(c) + '\n'
+            ret += '    ' + repr(c) + '\n'
+        return ret
 
 class Parser(object):
-    '''Responsible for taking raw text input and generating a list of tokens.'''
-
+    '''Parse code into AST'''
     @classmethod
     def lexical_analysis(cls, text:str) -> list:
+        '''Responsible for taking raw text input and generating a list of tokens.'''
         # buffers = list(filter(lambda x: x != '' and x != ' ', buffers))
         if DEBUG: print(text)
         buffers = []
         tokens = []
         buffer = ''
         i = 0
+        current_line_number = 1
         in_str_lit:bool = False
         in_comment:bool = False
         while i < len(text):
             curr_char = text[i]
-            next_char = text[i+1] if i+1<len(text) else None
-            prev_char = text[i-1] if i-1>=0 else None
+            # next_char = text[i+1] if i+1<len(text) else None
+            # prev_char = text[i-1] if i-1>=0 else None
             buffer += curr_char
             if not in_comment:
                 if curr_char == COMMENT_DELIM and not in_str_lit:
                     in_comment = True
                     if buffer[:-1] != '':
-                        tokens.append(cls.analyze_buffer(buffer[:-1]))
-                    
+                        tokens.append(cls.analyze_buffer(buffer[:-1], current_line_number))
                 elif curr_char == ' ' and not in_str_lit:
                     if buffer[:-1] != '':
-                        tokens.append(cls.analyze_buffer(buffer[:-1]))
+                        tokens.append(cls.analyze_buffer(buffer[:-1], current_line_number))
                     buffer = ''
                 elif curr_char == '(':
                     if buffer[:-1] != '' and not in_str_lit:
-                        tokens.append(cls.analyze_buffer(buffer[:-1]))
-                    tokens.append(cls.analyze_buffer('('))
+                        tokens.append(cls.analyze_buffer(buffer[:-1], current_line_number))
+                    tokens.append(cls.analyze_buffer('(', current_line_number))
                     buffer = ''
                 elif curr_char == ')':
                     if buffer[:-1] != '' and not in_str_lit:
-                        tokens.append(cls.analyze_buffer(buffer[:-1]))
-                    tokens.append(cls.analyze_buffer(')'))
+                        tokens.append(cls.analyze_buffer(buffer[:-1], current_line_number))
+                    tokens.append(cls.analyze_buffer(')', current_line_number))
                     buffer = ''
                 elif curr_char == ',' and not in_str_lit:
                     if buffer[:-1] != '' and not in_str_lit:
-                        tokens.append(cls.analyze_buffer(buffer[:-1]))
-                    tokens.append(Token(TOKEN_TYPE.COMMA))
+                        tokens.append(cls.analyze_buffer(buffer[:-1], current_line_number))
+                    tokens.append(Token(TOKEN_TYPE.COMMA, current_line_number))
                     buffer = ''
                 elif curr_char == '\"':
                     if not in_str_lit:
                         if buffer[:-1] != '':
-                            tokens.append(cls.analyze_buffer(buffer[:-1]))
+                            tokens.append(cls.analyze_buffer(buffer[:-1], current_line_number))
                         buffer = '\"'
                     else:
-                        tokens.append(cls.analyze_buffer(buffer))
+                        tokens.append(cls.analyze_buffer(buffer, current_line_number))
                         buffer = ''
                     in_str_lit = not in_str_lit
             if curr_char == '\n':
                 if buffer[:-1] != '' and not in_comment:
-                    tokens.append(cls.analyze_buffer(buffer[:-1]))
-                tokens.append(Token(TOKEN_TYPE.LINE_BREAK))
+                    tokens.append(cls.analyze_buffer(buffer[:-1], current_line_number))
+                tokens.append(Token(TOKEN_TYPE.LINE_BREAK, current_line_number))
                 buffer = ''
                 in_str_lit = False
                 in_comment = False
+                current_line_number += 1
             elif i == len(text) - 1:
                 if buffer != '':
                     tokens.append(cls.analyze_buffer(buffer))
@@ -186,12 +195,12 @@ class Parser(object):
         if DEBUG: pprint(buffers)
         for buffer in buffers:
             cls.analyze_buffer(buffer, tokens)
-        tokens.append(Token(TOKEN_TYPE.EOF))
+        tokens.append(Token(TOKEN_TYPE.EOF, current_line_number))
         if DEBUG: pprint(tokens)
         return tokens
 
     @classmethod
-    def analyze_buffer(cls, buffer:str) -> Token:
+    def analyze_buffer(cls, buffer:str, line_number:int) -> Token:
         matched_token:typing.Optional[TOKEN_TYPE] = None
         for tt in TOKEN_TYPE:
             pattern = tt.value
@@ -202,29 +211,55 @@ class Parser(object):
         if matched_token is None:
             raise RuntimeError(f"Unable to match token <{buffer}>")
         if tt in REQUIRES_VALUE:
-            token = Token(matched_token, value=buffer)
+            token = Token(matched_token, line_number, value=buffer)
         else:
-            token = Token(matched_token)
+            token = Token(matched_token, line_number)
         return token
+
 
     @classmethod
     def syntax_analysis(cls, tokens:list):
-
+        '''Take list of tokens and create a (potentially illegal) AST'''
         i = 0
-        if tokens[0] is not TOKEN_TYPE.PROGRAM or tokens[1] is not TOKEN_TYPE.STR_LIT:
+        def expect(*types):
+            j = 0
+            while j < len(types):
+                if not tokens[i+1+j].type != types[j]:
+                    raise SyntaxError(f"Expected token {types[j]} after {tokens[i]}, got {tokens[i+1+j]} instead.")
+                j += 1
+
+        if tokens[0].type is not TOKEN_TYPE.PROGRAM or tokens[1].type is not TOKEN_TYPE.STR_LIT:
             raise SyntaxError(f"Program must begin with a program name, got {tokens[0]} and {tokens[1]} instead")
 
         root_node = Node(TOKEN_TYPE.PROG, [])
-        expected:typing.Optional[TOKEN_TYPE] = None
         while i < len(tokens):
             curr = tokens[i]
             next = tokens[i+1] if i+1<len(tokens) else None
             if curr.type == TOKEN_TYPE.PROGRAM:
-                if i != 1: pass
-                if 
-            elif curr.type == 
-                
-            i += 1
+                if i != 0: raise SyntaxError("PROGRAM token must be first in program")
+                if next.type != TOKEN_TYPE.STR_LIT:
+                    raise SyntaxError(f"Expected STR_LIT, got {next.type}")
+                root_node.append(Node(curr, [next]))
+                i += 2 # digest next token
+            elif curr.type == TOKEN_TYPE.VERSION:
+                if next.type != TOKEN_TYPE.STR_LIT: raise SyntaxError("Expected STR_LIT after PROGRAM") 
+                if not all([tokens[j].type == TOKEN_TYPE.INT_LIT for j in range(i, i+3)]):
+                    raise SyntaxError("Expected INT_LIT after VERSION token")
+                root_node.append(Node(curr), [tokens[j] for j in range(i, i+3)])
+                i += 4
+            elif curr.type == TOKEN_TYPE.LABEL:
+                if next.type != TOKEN_TYPE.VAR: raise SyntaxError("Expected VAR after LABEL")
+                root_node.append(Node(curr, [next]))
+                i += 2
+            elif curr.type == TOKEN_TYPE.GOTO:
+                if next.type != TOKEN_TYPE.VAR: raise SyntaxError("Expected VAR after GOTO")
+                root_node.append(Node(curr, [next]))
+            elif curr.type == TOKEN_TYPE.IF: pass
+                # if tokens[i+1].type != @TODO
+            else:
+                if DEBUG: print(f'failed to parse token {curr}.')
+                i += 1
+        return root_node
 
 class Interpreter(object):
     def __init__(self):
@@ -236,20 +271,20 @@ class Interpreter(object):
 
 
     pass
-    
+
 VERSION:tuple = (0, 1, 0)
 def main() -> None:
     logger:logging.Logger = logging.getLogger()
-    
+
     # Argument Parser
     parser = argparse.ArgumentParser(description=f'TYTHON Compiler {VERSION}')
     parser.add_argument('input_file')
     parser.add_argument('-o', '--output')
     parser.add_argument('-c', '--compile', action='store_true', default=False)
     parser.add_argument('-i', '--interpret', action='store_true', default=False)
-    
+
     parser.add_argument('--debug', action='store_true', default=False)
-    parser.add_argument('-v', '--version', action='version', 
+    parser.add_argument('-v', '--version', action='version',
                         version=f'Tython {VERSION[0]}.{VERSION[1]}.{VERSION[2]}')
     args = parser.parse_args()
     filepath:pathlib.Path = pathlib.Path(args.input_file)
@@ -261,7 +296,7 @@ def main() -> None:
 
     # Configure compilation setting
     global COMPILE
-    if args.compile and args.interpret: 
+    if args.compile and args.interpret:
         raise SyntaxError("Cannot compile and interpret simultaneously")
     if not (args.compile or args.interpret):
         COMPILE = True
@@ -277,6 +312,7 @@ def main() -> None:
     # Initialize lexer
     tokens = Parser.lexical_analysis(file_contents)
     tree = Parser.syntax_analysis(tokens)
+    print(tree)
     pass
 
 if __name__ == '__main__':
