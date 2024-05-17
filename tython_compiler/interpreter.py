c4df3e85c214dd0665bad3b174a80d6c044eb092
ly49nkallo <tyabrennan@gmail.com>
2024-05-17 00:58:05 -0700

    stuff related to interpreting

--- tython_compiler/interpreter.py
+++ tython_compiler/interpreter.py
@@ -24,47 +24,96 @@ class Datatypes(enum.Enum):
     REAL64 = enum.auto()
     CHAR8 = enum.auto()
 
+def match_token_to_datatype(token) -> Datatypes:
+    assert isinstance(token, Token)
+    t = token.type
+    if t == TOKEN_TYPE.INT32:
+        return Datatypes.INT32
+    elif t == TOKEN_TYPE.INT64:
+        return Datatypes.INT64
+    elif t == TOKEN_TYPE.REAL32:
+        return Datatypes.REAL32
+    elif t == TOKEN_TYPE.REAL64:
+        return Datatypes.REAL64
+    elif t == TOKEN_TYPE.CHAR8:
+        return Datatypes.CHAR8
+    else:
+        raise ValueError("Token is not matched to interpreter datatype")
+    
+def get_default_type(var:str) -> Datatypes:
+    assert isinstance(var, str)
+    letter = var[0].upper()
+    assert letter.isalpha()
+    if letter in 'IJKLMN':
+        return Datatypes.INT32
+    else:
+        return Datatypes.REAL32
+
 class DType(abc.ABC):
     '''Abstract Data Type Base-Class'''
+
+    @property
     @abc.abstractmethod
-    def _get_data(self):
+    def _size() -> int:
         ...
 
+    @property
     @abc.abstractmethod
-    def _set_data(self, value):
+    def _data() -> typing.Optional[bytearray]:
         ...
 
+    @property
     @abc.abstractmethod
-    def _del_data(self):
-        raise NameError("Cannot delete intrinsic data value")
-    data = property(_get_data, _set_data, _del_data)
+    def _meta_dtype():
+        ...
 
-class IntegerDType(DType):
-    '''Base-Class for all Integer-like Datatypes'''
+class Integer(DType):
+    def add(self, other:'Integer'):
+        ...
+    def subtract(self, other:'Integer'):
+        ...
+    def negate(self):
+        ...
+    def multiply(self, other:'Integer'):
+        ...
+    def devide(self, other:'Integer'):
+        ...
+
+class Integer32(Integer):
+    _size = 4
+    _data = bytearray(_size)
+    _meta_dtype = Datatypes.INT32
     ...
 
-class Integer32(IntegerDType):
+class Integer64(Integer):
+    _size = 8
+    _data = bytearray(_size)
     ...
 
-class Integer64(IntegerDType):
-    ...
+class Float(DType):
+    def add(self, other:'Float'):
+        ...
+    def subtract(self, other:'Float'):
+        ...
+    def negate(self):
+        ...
+    def multiply(self, other:'Float'):
+        ...
+    def devide(self, other:'Float'):
+        ...
 
-class FloatDType(DType):
-    '''Base-Class for all Float-like Datatypes'''
-    ...
+class Float32(Float):
+    _size = 4
+    _data = bytearray(_size)
 
-class Float32(FloatDType):
-    ...
+class Float64(Float):
+    _size = 8
+    _data = bytearray(_size)
 
-class Float64(FloatDType):
-    ...
 
-class CharDType(DType):
-    '''Base-Class for all Char-like Datatypes'''
-    ...
-
-class Char8(CharDType):
-    ...
+class Char8(DType):
+    _size = 1
+    _data = bytearray(_size)
 
 class Interpreter():
     """Define an interpreter to handle code execution"""
@@ -85,3 +134,28 @@ class Interpreter():
         assert root_node.token.type == TOKEN_TYPE.PROG
         program_name:str = tree.children[0].children[0].token.value
         print(f'{program_name=}')
+        cls.interpret_block(root_node)
+
+    @classmethod
+    def assign_datatype(cls, var:str, dtype:Datatypes):
+        assert isinstance(var, str) and isinstance(dtype, Datatypes), f"{var.type=}, {dtype.type=}"
+        cls.type_map[var.upper()] = dtype
+    
+    @staticmethod
+    def evaluate_expression(expression_root_node:Node) -> typing.Union[Integer32, Integer64, Float32, Float64]:
+        ...
+
+    @classmethod
+    def interpret_block(cls, root_node:Node):
+        for node in root_node.children:
+            assert isinstance(node, Node)
+            token = node.token
+            children = node.children
+            if token.type == TOKEN_TYPE.IMPLICIT:
+                dtype = match_token_to_datatype(children[0].token)
+                var = children[1].token.value
+                cls.assign_datatype(var, dtype)
+                print(cls.type_map)
+            elif token.type == TOKEN_TYPE.ASSIGN:
+                var = children[0].token.value
+
